f4722a396f80b9401589f448f6f1e3dd64ee3475
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-10-31 11:08:31 +0100

    First principally working version.

--- core/run.sh
+++ core/run.sh
@@ -4,12 +4,13 @@
 # Github repository: https://github.com/MHBalsmeier/ndvar
 
 echo "This is ndvar."
+echo "Copyright (C) 2020 The ndvar development team."
 echo "Starting the assimilation process ..."
-echo "analysis year: "$year
-echo "analysis month: "$month
-echo "analysis day: "$day
-echo "analysis hour: "$hour
-mpirun -np 1 bin/ndvar $year $month $day $hour $model_home_directory
+echo "analysis year: "$analysis_year
+echo "analysis month: "$analysis_month
+echo "analysis day: "$analysis_day
+echo "analysis hour: "$analysis_hour
+mpirun -np 1 bin/ndvar $analysis_year $analysis_month $analysis_day $analysis_hour $model_home_directory
 if [ $? -ne 0 ]
 then
 echo -e ${RED}Data assimilation failed.$NC
--- run_script.sh
+++ run_script.sh
@@ -3,15 +3,11 @@
 # This source file is part of ndvar, which is released under the MIT license.
 # Github repository: https://github.com/MHBalsmeier/ndvar
 
-year=${BASH_ARGV[0]}
-month=${BASH_ARGV[1]}
-day=${BASH_ARGV[2]}
-hour=${BASH_ARGV[3]}
-model_home_directory=${BASH_ARGV[4]}
-
-cycle=(0 6 12 18) # the UTC times of the analyses
-
-model_home_directory=/home/max/compiled/game_dev # The directory of the model.
+analysis_year=${BASH_ARGV[4]}
+analysis_month=${BASH_ARGV[3]}
+analysis_day=${BASH_ARGV[2]}
+analysis_hour=${BASH_ARGV[1]}
+model_home_directory=${BASH_ARGV[0]}
 
 # That's it, now the assimilation process will be started.
 source bin/run.sh
