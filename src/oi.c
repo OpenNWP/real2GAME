52162337545b9949a3da609b8a968ff1dedaa51e
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-24 00:09:49 +0200

    OMP largely disabled

--- src/oi.c
+++ src/oi.c
@@ -18,7 +18,7 @@ int oi(double obs_error_cov[], double obs_op_jacobian_reduced_matrix[][NO_OF_REL
 	double (*h_b_ht_plus_r)[no_of_obs] = malloc(sizeof(double[no_of_obs][no_of_obs]));
 	int index_found;
 	int check_vector[NO_OF_REL_MODEL_DOFS_PER_OBS];
-    #pragma omp parallel for private(index_found, check_vector)
+    //#pragma omp parallel for private(index_found, check_vector)
 	for (int i = 0; i < no_of_obs; ++i)
 	{
 		for (int l = 0; l < NO_OF_REL_MODEL_DOFS_PER_OBS; ++l)
@@ -85,7 +85,7 @@ int oi(double obs_error_cov[], double obs_op_jacobian_reduced_matrix[][NO_OF_REL
 	// this vector will contain the product of the model forecast error and the gain matrix
 	double *prod_with_gain_matrix = calloc(no_of_model_dofs, sizeof(double));
 	// multiplying (obs - (interpolated model)) by the gain matrix
-    #pragma omp parallel for
+    //#pragma omp parallel for
 	for (int i = 0; i < NO_OF_REL_MODEL_DOFS_PER_OBS; ++i)
 	{
 		for (int j = 0; j < no_of_obs; ++j)
