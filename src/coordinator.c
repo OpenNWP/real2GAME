4e237a87bbd2094002b89ea398453783294bf894
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-29 12:48:35 +0200

    comment changed

--- src/coordinator.c
+++ src/coordinator.c
@@ -439,7 +439,6 @@ int main(int argc, char *argv[])
 	#pragma omp parallel for
 	for (int i = 0; i < NO_OF_SCALARS; ++i)
 	{
-		// water vapour density
 		water_vapour_density[i] = model_vector_moist[i]/(1 - model_vector_moist[i])*density_dry[i];
 		if (water_vapour_density[i] < 0)
 		{
