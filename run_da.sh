35d6e6eac529c84c1755e70c32a412d7598c6d90
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-09-23 13:15:36 +0200

    formal changes

--- run_da.sh
+++ run_da.sh
@@ -21,9 +21,9 @@ export OMP_NUM_THREADS=$omp_num_threads # relevant only for OMP
 echo "This is GAME-DA."
 echo "Starting the assimilation process ..."
 echo "analysis year: "$analysis_year
-echo "analysis month: "$analysis_month
-echo "analysis day: "$analysis_day
-echo "analysis hour: "$analysis_hour
+echo "Analysis month: "$analysis_month
+echo "Analysis day: "$analysis_day
+echo "Analysis hour: "$analysis_hour
 $game_da_root_dir/build/game-da $analysis_year $analysis_month $analysis_day $analysis_hour $model_home_directory $orography_id $background_file $game_da_root_dir $orography_layers $toa
 if [ $? -ne 0 ]
 then
