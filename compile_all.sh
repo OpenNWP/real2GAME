109996127b35ca00558b4c4e72d765242b39a7d4
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-18 01:29:49 +0100

    formal changes

--- compile_all.sh
+++ compile_all.sh
@@ -3,7 +3,7 @@
 # This source file is part of real2GAME, which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/real2GAME
 
-./compile_da.sh
+./compile_real2game.sh
 
 cd formatter
 ./compile_formatter.sh
