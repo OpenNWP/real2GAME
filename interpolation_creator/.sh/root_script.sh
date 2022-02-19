90dc281f221b00761765216578ea1524fce676f4
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-19 13:00:20 +0100

    formal changes

--- interpolation_creator/.sh/root_script.sh
+++ interpolation_creator/.sh/root_script.sh
@@ -3,4 +3,4 @@
 # This source file is part of real2GAME, which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/real2GAME
 
-source .sh/root_script.sh
+./interpolation_creator 
