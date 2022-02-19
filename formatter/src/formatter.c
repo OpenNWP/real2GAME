513afdb82e51d0cce596fd441fa04fde5c8ceee6
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-19 15:55:15 +0100

    formatter improved

--- formatter/src/formatter.c
+++ formatter/src/formatter.c
@@ -17,12 +17,18 @@ int main(int argc, char *argv[])
 {
 	// defining the levels of the model we want to use
 	int levels_vector[NO_OF_LEVELS_OBS];
-	levels_vector[0] = 24;
-	levels_vector[1] = 37;
-	levels_vector[2] = 50;
-	levels_vector[3] = 63;
-	levels_vector[4] = 77;
-	levels_vector[5] = 90;
+	levels_vector[0] = 1;
+	levels_vector[1] = 10;
+	levels_vector[2] = 19;
+	levels_vector[3] = 27;
+	levels_vector[4] = 35;
+	levels_vector[5] = 43;
+	levels_vector[6] = 51;
+	levels_vector[7] = 59;
+	levels_vector[8] = 67;
+	levels_vector[9] = 75;
+	levels_vector[10] = 83;
+	levels_vector[11] = 91;
 	
 	// shell arguments
     char year_string[strlen(argv[1]) + 1];
@@ -46,7 +52,6 @@ int main(int argc, char *argv[])
 	double *latitude_vector = malloc(NO_OF_CHOSEN_OBSERVATIONS*sizeof(double));
 	double *longitude_vector = malloc(NO_OF_CHOSEN_OBSERVATIONS*sizeof(double));
 	double *z_coords_amsl = malloc(NO_OF_CHOSEN_OBSERVATIONS*sizeof(double));
-	double *observations_vector = malloc(NO_OF_CHOSEN_OBSERVATIONS*sizeof(double));
 	
 	double *temperature_one_layer = malloc(NO_OF_POINTS_PER_LAYER_OBS*sizeof(double));
 	double *u_wind_one_layer = malloc(NO_OF_POINTS_PER_LAYER_OBS*sizeof(double));
@@ -61,7 +66,8 @@ int main(int argc, char *argv[])
 	{
 		// vertical position of the current layer
 		char Z_OBS_FILE_PRE[200];
-		sprintf(Z_OBS_FILE_PRE , "%s/input/icon_global_icosahedral_time-invariant_%s%s%s%s_%d_HHL.grib2", real2game_root_dir, year_string, month_string, day_string, hour_string, levels_vector[level_index]);
+		sprintf(Z_OBS_FILE_PRE , "%s/input/icon_global_icosahedral_time-invariant_%s%s%s%s_%d_HHL.grib2",
+		real2game_root_dir, year_string, month_string, day_string, hour_string, levels_vector[level_index]);
 		char Z_OBS_FILE[strlen(Z_OBS_FILE_PRE) + 1];
 		strcpy(Z_OBS_FILE, Z_OBS_FILE_PRE);
 		
@@ -144,32 +150,26 @@ int main(int argc, char *argv[])
 		fclose(ECC_FILE);
 		
 		// formatting the observations
-		#pragma omp parallel for private(shift_index)
+		#pragma omp parallel for
 		for (int i = 0; i < NO_OF_CHOSEN_POINTS_PER_LAYER_OBS; ++i)
 		{
 			
 			// temperature
 			shift_index = level_index*NO_OF_CHOSEN_POINTS_PER_LAYER_OBS;
 			z_coords_amsl[shift_index + i] = z_height_amsl[chosen_indices[i]];
-			observations_vector[shift_index + i] = temperature_one_layer[chosen_indices[i]];
 			
 			// specific humidity
 			shift_index = NO_OF_CHOSEN_OBSERVATIONS_MOIST + level_index*NO_OF_CHOSEN_POINTS_PER_LAYER_OBS;
 			z_coords_amsl[shift_index + i] = z_height_amsl[chosen_indices[i]];
-			observations_vector[shift_index + i] = spec_hum_one_layer[chosen_indices[i]];
 		}
-		#pragma omp parallel for private(shift_index)
+		#pragma omp parallel for
 		for (int i = 0; i < NO_OF_CHOSEN_WIND_POINTS_PER_LAYER_OBS; ++i)
 		{
 			// u wind
-			shift_index = NO_OF_CHOSEN_OBSERVATIONS_DRY + NO_OF_CHOSEN_OBSERVATIONS_MOIST + level_index*NO_OF_CHOSEN_WIND_POINTS_PER_LAYER_OBS;
 			z_coords_amsl[shift_index + i] = z_height_amsl[chosen_indices_wind[i]];
-			observations_vector[shift_index + i] = u_wind_one_layer[chosen_indices_wind[i]];
 			
 			// v wind
-			shift_index = NO_OF_CHOSEN_OBSERVATIONS_DRY + NO_OF_CHOSEN_OBSERVATIONS_MOIST + NO_OF_CHOSEN_OBSERVATIONS_WIND/2 + level_index*NO_OF_CHOSEN_WIND_POINTS_PER_LAYER_OBS;
 			z_coords_amsl[shift_index + i] = z_height_amsl[chosen_indices_wind[i]];
-			observations_vector[shift_index + i] = v_wind_one_layer[chosen_indices_wind[i]];
 		}
 	}
 	free(z_height_amsl);
@@ -197,7 +197,7 @@ int main(int argc, char *argv[])
 	
 	// surface pressure
 	// reading the surface presure
-	double *pressure_one_layer = malloc(NO_OF_POINTS_PER_LAYER_OBS*sizeof(double));
+	double *pressure_surface = malloc(NO_OF_POINTS_PER_LAYER_OBS*sizeof(double));
 	
 	char SFC_PRES_FILE_PRE[200];
 	sprintf(SFC_PRES_FILE_PRE , "%s/input/icon_global_icosahedral_single-level_%s%s%s%s_000_PS.grib2", real2game_root_dir, year_string, month_string, day_string, hour_string);
@@ -209,7 +209,7 @@ int main(int argc, char *argv[])
 	if (err != 0)
 		ECCERR(err);
 	NO_OF_POINTS_PER_LAYER_OBS_SIZE_T = (size_t) NO_OF_POINTS_PER_LAYER_OBS;
-	if ((retval = codes_get_double_array(handle, "values", &pressure_one_layer[0], &NO_OF_POINTS_PER_LAYER_OBS_SIZE_T)))
+	if ((retval = codes_get_double_array(handle, "values", &pressure_surface[0], &NO_OF_POINTS_PER_LAYER_OBS_SIZE_T)))
 		ECCERR(retval);
 	codes_handle_delete(handle);
 	fclose(ECC_FILE);
@@ -220,7 +220,7 @@ int main(int argc, char *argv[])
 	{
 		shift_index = NO_OF_LEVELS_OBS*2*NO_OF_CHOSEN_POINTS_PER_LAYER_OBS;
 		z_coords_amsl[shift_index + i] = surface_height[chosen_indices[i]];
-		observations_vector[shift_index + i] = pressure_one_layer[chosen_indices[i]];
+		observations_vector[shift_index + i] = pressure_surface[chosen_indices[i]];
 	}
 	
 	// SST
@@ -247,14 +247,12 @@ int main(int argc, char *argv[])
 	fclose(ECC_FILE);
 	
 	// writing the SST to the observations
-	#pragma omp parallel for private(shift_index)
+	#pragma omp parallel for
 	for (int i = 0; i < NO_OF_SST_POINTS; ++i)
 	{
-		shift_index = NO_OF_CHOSEN_OBSERVATIONS - NO_OF_SST_POINTS;
 		latitude_vector[shift_index + i] = 2*M_PI*latitudes_sst[i]/360;
 		longitude_vector[shift_index + i] = 2*M_PI*longitudes_sst[i]/360;
 		z_coords_amsl[shift_index + i] = 0;
-		observations_vector[shift_index + i] = sst[i];
 	}
 	
 	// freeing the memory
@@ -262,8 +260,7 @@ int main(int argc, char *argv[])
 	free(latitudes_sst);
 	free(longitudes_sst);
 	free(surface_height);
-	free(pressure_one_layer);
-    free(chosen_indices);
+	free(pressure_surface);
     
     // Writing the observations to a netcdf file.
     char OUTPUT_FILE_PRE[200];
@@ -271,34 +268,48 @@ int main(int argc, char *argv[])
 	char OUTPUT_FILE[strlen(OUTPUT_FILE_PRE) + 1];
 	strcpy(OUTPUT_FILE, OUTPUT_FILE_PRE);
     
-    int ncid, observation_dimid, latitude_id, longitude_id, vert_id, obervations_id;
+    int ncid, h_dimid, v_dimid, sst_dimid, t_id, spec_hum_id, sp_id, u_id, v_id, sst_id;
+    int dim_vector[2];
     if ((retval = nc_create(OUTPUT_FILE, NC_CLOBBER, &ncid)))
         NCERR(retval);
     // Defining the dimensions.
-    if ((retval = nc_def_dim(ncid, "observation_index", NO_OF_CHOSEN_OBSERVATIONS, &observation_dimid)))
+    if ((retval = nc_def_dim(ncid, "h_index", NO_OF_POINTS_PER_LAYER_OBS, &h_dimid)))
         NCERR(retval);
-    // Defining the variables.
-    if ((retval = nc_def_var(ncid, "latitude_vector", NC_DOUBLE, 1, &observation_dimid, &latitude_id)))
+    if ((retval = nc_def_dim(ncid, "v_index", NO_OF_LEVELS_OBS, &v_dimid)))
         NCERR(retval);
-    if ((retval = nc_def_var(ncid, "longitude_vector", NC_DOUBLE, 1, &observation_dimid, &longitude_id)))
+    if ((retval = nc_def_dim(ncid, "sst_index", NO_OF_SST_POINTS, &sst_dimid)))
         NCERR(retval);
-    if ((retval = nc_def_var(ncid, "z_coords_obs", NC_DOUBLE, 1, &observation_dimid, &vert_id)))
+    // Defining the variables."interpol_index", NO_OF_AVG_POINTS, &avg_dimid)))
         NCERR(retval);
-    if ((retval = nc_def_var(ncid, "observations_vector", NC_DOUBLE, 1, &observation_dimid, &obervations_id)))
+    dim_vector[0] = h_dimid;
+    dim_vector[1] = v_dimid;
+    if ((retval = nc_def_var(NC_DOUBLE, "pressure_surface", NC_INT, 1, &h_dimid, &sp_id)))
         NCERR(retval);
-    
+    if ((retval = nc_def_var(ncid, "temperature", NC_DOUBLE, 2, dim_vector, &t_id)))
+        NCERR(retval);
+    if ((retval = nc_def_var(ncid, "spec_hunidity", NC_DOUBLE, 2, dim_vector, &spec_hum_id)))
+        NCERR(retval);
+	if ((retval = nc_def_var(ncid, "u_wind", NC_DOUBLE, 2, dim_vector, &u_id)))
+	  	NCERR(retval);
+	if ((retval = nc_def_var(ncid, "v_wind", NC_DOUBLE, 2, dim_vector, &v_id)))
+	  	NCERR(retval);
+	if ((retval = nc_def_var(ncid, "sst", NC_DOUBLE, 1, &sst_dimid, &sst_id)))
+	  	NCERR(retval);
     if ((retval = nc_enddef(ncid)))
         NCERR(retval);
-        
     // Setting the variables.
-    if ((retval = nc_put_var_double(ncid, latitude_id, &latitude_vector[0])))
-        NCERR(retval);
-    if ((retval = nc_put_var_double(ncid, longitude_id, &longitude_vector[0])))
-        NCERR(retval);
-    if ((retval = nc_put_var_double(ncid, vert_id, &z_coords_amsl[0])))
-        NCERR(retval);    
-    if ((retval = nc_put_var_double(ncid, obervations_id, &observations_vector[0])))
-        NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, sp_id, &pressure_surface[0])))
+	  	NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, t_id, &temperature[0][0])))
+	  	NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, t_id, &spec_hum[0][0])))
+	  	NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, u_id, &u_wind[0][0])))
+	  	NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, v_id, &w_ind[0][0])))
+	  	NCERR(retval);
+	if ((retval = nc_put_var_double(ncid, sst_id, &sst[0])))
+	  	NCERR(retval);
     if ((retval = nc_close(ncid)))
     	NCERR(retval);
     
@@ -307,7 +318,16 @@ int main(int argc, char *argv[])
 	free(latitude_vector);
 	free(longitude_vector);
 	free(z_coords_amsl);
-	free(observations_vector);
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
