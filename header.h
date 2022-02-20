03662836eddea443761ad0a3bba161e01aa358fa
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-20 12:03:12 +0100

    less vertical levels

--- header.h
+++ header.h
@@ -4,7 +4,7 @@ Github repository: https://github.com/OpenNWP/real2GAME
 */
 
 // the number of levels from which we use observations
-#define NO_OF_LEVELS_INPUT 23
+#define NO_OF_LEVELS_INPUT 12
 // values smaller than two smooth the fields
 #define INTERPOL_EXP 2.001
 // number of points used for averaging
