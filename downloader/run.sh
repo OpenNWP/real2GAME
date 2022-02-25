40398ae9c7915ec57aa64ca0bc314b9474dba256
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-02-25 13:21:00 +0100

    model source ID introduced

--- downloader/run.sh
+++ downloader/run.sh
@@ -8,6 +8,22 @@ analysis_year=${BASH_ARGV[3]}
 analysis_month=${BASH_ARGV[2]}
 analysis_day=${BASH_ARGV[1]}
 analysis_hour=${BASH_ARGV[0]}
+model_source_id=${BASH_ARGV[5]}
 
+if [ $model_source_id -eq 0 ]
+then
 source $real2game_root_dir/downloader/dl_icon-global.sh
+fi
+
+if [ $model_source_id -eq 1 ]
+then
+source $real2game_root_dir/downloader/dl_icon-eu.sh
+fi
+
+if [ $model_source_id -eq 2 ]
+then
+source $real2game_root_dir/downloader/dl_icon-d2.sh
+fi
+
+# downloading SST
 source $real2game_root_dir/downloader/dl_sst.sh
