9c5f1cca1965c1397faad9b8b1ff6614f5065ee6
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-04-24 10:22:24 +0200

    run script changed

--- interpolation_creator/run.sh
+++ interpolation_creator/run.sh
@@ -6,8 +6,8 @@
 real2game_root_dir=/home/max/code/real2GAME # the home directory of real2GAME
 model_home_dir=/home/max/code/GAME # the home directory of GAME or L-GAME
 model_target_id=0 # 0: GAME 1: L-GAME
-nlat=25 # number of latitude points of the L-GAME grid
-nlon=25 # number of longitude points of the L-GAME grid
+nlat=35 # number of latitude points of the L-GAME grid
+nlon=35 # number of longitude points of the L-GAME grid
 interpol_exp=2.001 # interpolation exponent
 lgame_grid="grid.nc" # grid filename of L-GAME
 oro_id=1 # orography ID (relevant only for GAME)
