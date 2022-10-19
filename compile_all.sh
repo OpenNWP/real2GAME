c641e90d4d67a601f5da4d0ae3ecdfd777f49362
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-10-19 22:59:13 +0200

    more compilation options

--- compile_all.sh
+++ compile_all.sh
@@ -3,6 +3,22 @@
 # This source file is part of real2GAME, which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/real2GAME
 
+d_value=False
+f_value=False
+while getopts "df" opt; do
+  case $opt in
+    d) # debugging flag
+      d_value=True
+      ;;
+    f) # aggressive optimization flag
+      f_value=True
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG. Compiling anyway."
+      ;;
+  esac
+done
+
 # interpolation_creator build
 
 cd interpolation_creator
@@ -12,18 +28,6 @@ then
 fi
 
 cd build
-
-d_value=False
-while getopts "d" opt; do
-  case $opt in
-    d) # debugging flag
-      d_value=True
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG. Compiling anyway."
-      ;;
-  esac
-done
 
 cmake -DDEBUGGING=$d_value ..
 make
@@ -41,7 +45,7 @@ fi
 
 cd build
 
-cmake -DDEBUGGING=$d_value ..
+cmake -DDEBUGGING=$d_value -DFAST=$f_value ..
 make
 
 cd ..
@@ -56,7 +60,7 @@ fi
 
 cd build
 
-cmake -DDEBUGGING=$d_value ..
+cmake -DDEBUGGING=$d_value -DFAST=$f_value ..
 make
 
 cd ..
